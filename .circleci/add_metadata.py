d1bfa16a368e79b00fbb7324630eedbd986ece1c
Will Price <will.price94@gmail.com>
2019-04-09 13:20:43 +0100

    Update pickles with pandas==0.22.0 (#10)
    * Add metadata with pandas v0.23.4 as pickled dataframes aren't backwards compatible
    * Only pin circleci image to 3.6 instead of 3.6.1
    * Revert pickles to those from a687484891dfbc6a278a30a33139cab10a2a165c
    * Revert EPIC_train_object_action_correspondence.pkl to 3f31a91992884e2b0f90784e619972c21269e248
    since Mike updated that pkl in that commit so the index of the pickle
    matches the first column of the CSV in line with the rest of our other
    pickles.
    * Improvements to the testing script mean that we no longer need to
    specify the csv paths---we ensure a csv exists for every pickle.
    * Pickle dataframes with protocol 2 in add_metadata and revert to pandas==0.22.0 as specified in README
    * Improve assertions in test_pickles.py
    * Use git rev-parse instead of log for getting current commit hash

--- .circleci/add_metadata.py
+++ .circleci/add_metadata.py
@@ -10,16 +10,16 @@ def main(args):
         df = pd.read_pickle(path)
         metadata = {'commit_hash': args.commit_hash, 'version_number': args.version_number}
         df._metadata = metadata
-        pd.to_pickle(df, path)
+        pd.to_pickle(df, path, protocol=2)
     return 0
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Adds metadata to a pandas dataframe stored in a pickle')
     parser.add_argument('dataframe_paths', type=Path, nargs='+',
             help='Pickle file containing pandas dataframe to add metadata to')
-    parser.add_argument('--commit-hash', type=str, 
+    parser.add_argument('--commit-hash', type=str,
             help='Commit hash metadata to store in pandas dataframe.')
-    parser.add_argument('--version-number', type=str, 
+    parser.add_argument('--version-number', type=str,
             help='Version Number metadata to store in pandas dataframe.')
 
     args = parser.parse_args()
--- .circleci/test_pickles.py
+++ .circleci/test_pickles.py
@@ -6,39 +6,62 @@ from pathlib import Path
 
 
 def test_files(pkl_paths, csv_paths, commit_hash, version_number):
-    for i, p_file in enumerate(pkl_paths):
-        c_file = csv_paths[i]
-        csv = pd.read_csv(c_file)
-        df = pd.read_pickle(p_file)
-        assert df._metadata['commit_hash'] == commit_hash
-        assert df._metadata['version_number'] == version_number
-        assert len(csv) == len(df)
-        csv_uid = set(csv[csv.columns[0]])
-        df_uid = set(df.index)
-        assert csv_uid == df_uid
+    for pkl_path, csv_path in zip(pkl_paths, csv_paths):
+        csv = pd.read_csv(csv_path)
+        df = pd.read_pickle(pkl_path)
+        assert_equal(commit_hash, df._metadata['commit_hash'])
+        assert_equal(version_number, df._metadata['version_number'])
+        assert_equal(len(df), len(csv))
+        csv_ids = set(csv[csv.columns[0]])
+        df_ids = set(df.index)
+        assert csv_ids == df_ids, get_csv_error_message(pkl_path, csv_path, csv_ids, df_ids)
+
+def get_csv_error_message(csv_path, pkl_path, csv_ids, df_ids):
+    in_csv_not_in_df = csv_ids - df_ids
+    in_df_not_in_csv = df_ids - csv_ids
+    message = ""
+    if len(in_csv_not_in_df) > 0:
+        message += f"IDs in CSV ({csv_path}) but not present in DataFrame ({pkl_path}):\n"
+        message += missing_set_message(in_csv_not_in_df)
+    if len(in_df_not_in_csv) > 0:
+        if len(message) > 0:
+            message += "\n"
+        message += f"IDs in DataFrame ({pkl_path}) but not present in CSV ({csv_path}):\n"
+        message += missing_set_message(in_df_not_in_csv)
+    return message
+
+
+def missing_set_message(set_, max_items=50):
+    def elems_to_str(elems):
+        return ", ".join(map(str, elems))
+
+    if len(set_) <= max_items:
+        return elems_to_str(set_) + "\n"
+
+    as_list = list(set_)
+    return "{}, ..., {}".format(
+        elems_to_str(as_list[:max_items//2]),
+        elems_to_str(as_list[-max_items//2:])
+    ) + "\n"
+
+
+def assert_equal(expected, actual):
+    assert expected == actual, "Expected {} but was {}".format(expected, actual)
 
 
 def main(args):
-    get_file_names = lambda L: {str(l).split('/')[-1].split('.')[0] for l in L}
-    pickle_file_names = get_file_names(args.pickle_paths)
-    csv_file_names = get_file_names(args.csv_paths)
-    assert pickle_file_names == csv_file_names
-
-    pickle_file_paths = sorted(list(args.pickle_paths))
-    csv_file_paths = sorted(list(args.csv_paths))
-    test_files(pickle_file_paths, csv_file_paths, args.commit_hash, args.version_number)
+    pkl_paths = args.pickle_paths
+    csv_paths = [pkl_path.with_suffix('.csv') for pkl_path in pkl_paths]
+    test_files(pkl_paths, csv_paths, args.commit_hash, args.version_number)
 
 
-#git log --all --grep='skip' --max-count=1 | grep -oP 'pickles from* \K.*'
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Tests pickles can be read and the metadata exists')
     parser.add_argument('pickle_paths', type=Path, nargs='+',
             help='Pickle file containing pandas dataframe to add metadata to')
-    parser.add_argument('--csv-paths', required=True, type=Path, nargs='+',
-            help='Csv filepaths, should contain the same files as in the pickles')
-    parser.add_argument('--commit-hash', type=str, 
+    parser.add_argument('--commit-hash', type=str,
             help='Commit hash metadata to store in pandas dataframe.')
-    parser.add_argument('--version-number', type=str, 
+    parser.add_argument('--version-number', type=str,
             help='Version Number metadata to store in pandas dataframe.')
 
     args = parser.parse_args()
